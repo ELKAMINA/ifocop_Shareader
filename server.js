91556216f24b14965b66f8082af059fa75df43ff
Amina EL KHATTABI <aminaelk@macbook-pro-de-amina.home>
2022-01-26 14:40:32 +0100

    fixed untacked files

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const User = require("./models/User")
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const bcrypt = require("bcrypt");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 
 // dotenv.config() permet de configurer des adresses secrètes pour protéger les données.
 dotenv.config();
 
 // Enregistrer les sessions 
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
+// const session = require('express-session')
+// const cookieParser = require('cookie-parser')
 
 // Mongoose sert à modéliser des données. It acts as an intermediate between mongodb and server side language(like NodeJs). See this on stackoverflow : https://stackoverflow.com/questions/55604057 what-is-mongo-url-and-what-should-it-be-set-to
 mongoose
@@ -36,11 +36,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(morgan("common"));
-app.use(session({
-	secret:'test',
-	resave: false,
-	saveUninitialized: true
-}));
+// app.use(session({
+// 	secret:'test',
+// 	resave: false,
+// 	saveUninitialized: true
+// }));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
