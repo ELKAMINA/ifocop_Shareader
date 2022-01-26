bcc977d403adb4e9a1dc477ec0b98a682efcd402
Amina EL KHATTABI <aminaelk@macbook-pro-de-amina.home>
2022-01-26 14:12:58 +0100

    try

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser');
 
 
 // dotenv.config() permet de configurer des adresses secrètes pour protéger les données.
-dotenv.config();
+require("dotenv").config();
 
 // Enregistrer les sessions 
 const session = require('express-session')
